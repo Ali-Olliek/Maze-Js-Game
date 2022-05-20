31add6adb16f1e90517e0a7334904f1465ee8051
Ali <Ali@ADMINs-MacBook-Pro.local>
2022-05-20 06:35:13 +0300

    JS -- Added you_won  function

--- game.js
+++ game.js
@@ -15,7 +15,8 @@ start.addEventListener('click', function run_maze() {
 });
 
 function you_won() {
-
+    document.body.style.background = 'lightgreen'
+    document.getElementById('status').innerHTML = "YOU WON!"
 }
 
 function you_lost() {
