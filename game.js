6e70a58df36b7e06ac35f5765bb6f62dee0c54e8
Ali <Ali@ADMINs-MacBook-Pro.local>
2022-05-20 06:34:19 +0300

    JS -- Initialized variables

---
+++ game.js
@@ -0,0 +1,23 @@
+// Game structure
+// Press S --> Playing = true 
+// while playing --> if hover outside "game"--> You Lose!
+// if hover over boundery --> You Lose!
+// else if hover over E --> You win!
+
+var playing = false;
+var boundaries = document.getElementsByClassName('boundary');
+var start = document.getElementById('start');
+var end = document.getElementById('end')
+var status = document.getElementById('status')
+
+start.addEventListener('click', function run_maze() {
+
+});
+
+function you_won() {
+
+}
+
+function you_lost() {
+
+}
