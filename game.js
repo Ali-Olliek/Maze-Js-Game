e1e6855cd940a8f3200197728e383d664fdaba9c
AliOlliek <olliek95@gmail.com>
2022-05-20 21:43:44 +0300

    JS -- Preliminary working

--- game.js
+++ game.js
@@ -4,82 +4,31 @@
 // if hover over boundery --> You Lose!
 // else if hover over E --> You win!
 
-var playing= false;
 
+window.onload = function () {
 
-let boundaries = document.getElementsByName('boundary');
-    let start = document.getElementById('start');
-    let end = document.getElementById('end');
-    let game_prompt = document.getElementById('status');
-    let valid_play_ground = document.getElementById('game');
+    var boundaries = document.getElementsByClassName("boundary");
+    var start = document.getElementById('start');
+    var game_prompt = document.getElementById("status");
+    var end = document.getElementById('end');
 
-    start.addEventListener("mouseover", function (event) {
-        game_prompt.innerHTML = "GAME STARTED";
-        playing=true;
-        while (playing) {
-            boundaries.forEach(element => {
-                if(boundaries.addEventListener("mouseover",function(){
-                    document.body.style.backgroundColor = 'red',
-                    game_prompt.innerHTML = "YOU LOST! :(";
-                    playing=false;
-                }));
-                else if (end.addEventListener("mouseover", function(){
-                    document.body.style.backgroundColor = 'lightgreen';
-                    game_prompt.innerHTML = "YOU WON!";
-                    playing=false;
-                }));
-            })
-        }
-    });
-
-
-
-
-
-
-// function you_won() {
-//     document.body.style.backgroundColor = 'lightgreen';
-//     game_prompt.innerHTML = "YOU WON!";
-// }
-
-// function you_lost() {
-//     return document.body.style.backgroundColor = 'red',
-//     game_prompt.innerHTML = "YOU LOST! :(";
-// }
-
-//         if (boundaries.mouseover = function(){
-//             document.body.style.backgroundColor = 'red';
-//             document.getElementById('status').innerHTML = "YOU LOST! :(";
-//         }
-//         });
-//         else if (document.getElementById('end').mouseover= function(){
-//             document.body.style.backgroundColor = 'lightgreen';
-//             document.getElementById('status').innerHTML = "YOU WON!";
-//         });
-
-//     }
-// }
-// function run_maze() {
-//     document.getElementById('status').innerHTML = "GAME STARTED!";
-//     start.onclick = start_maze();
-// }
-
-// function start_maze() {
-//     document.getElementById('status').innerHTML = "GAME STARTED!";
-//     // if (document.getElementById('boundaries').onmouseover = you_lost());
     
-//     // else if (document.getElementById('end').onmouseover = you_won());
-// }
+    var run_maze = function (){
+        game_prompt.innerHTML = "GAME STARTED";
+        for (var i = 0; i < boundaries.length; i++) {
+        boundaries[i].addEventListener("mouseover", function(){
+         document.body.style.backgroundColor = "red";
+         game_prompt.innerHTML = "YOU LOST";
+         return
+        });
+        }
+        end.addEventListener("mouseover", function(){
+            document.body.style.backgroundColor = "lightgreen";
+            game_prompt.innerHTML = "YOU WON";
+            return
+        })
+        
+    }
 
-
-// function you_won() {
-//     document.body.style.background = 'lightgreen'
-//     document.getElementById('status').innerHTML = "YOU WON!"
-//     var win_audio = new Audio('assets/audio/super-mario-bros_DhUAiGM.mp3');
-// }
-
-// function you_lost() {
-//     document.body.style.background = 'red';
-//     document.getElementById('status').innerHTML = "YOU LOST! :(";
-//     var lost_audio = new Audio('assets/audio/super-mario-death-sound-sound-effect.mp3');
-// }
+    start.addEventListener('click', run_maze);
+};
