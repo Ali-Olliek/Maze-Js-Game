a3ce7cd4e603cee0e276d14c261032b9a704286c
AliOlliek <olliek95@gmail.com>
2022-05-21 00:44:34 +0300

    JS -- Trying to fix RemoEventListener

--- game.js
+++ game.js
@@ -13,44 +13,46 @@ window.onload = function () {
     var end = document.getElementById('end');
     var score = 0;
     var game = document.getElementById('game');
-    var not_playing = false;
+    var finished = false;
     
-    var run_maze = function (){
-        
+var run_maze = function (){
+    if (finished===true) {
+    for (var i = 0; i < boundaries.length; i++) {
+      boundaries[i].removeEventListener("mouseover", you_lost, false);
+    }
+    end.removeEventListener("mouseover", you_won, false);
+}
         game.addEventListener("mouseleave", you_lost);
 
         game_prompt.innerHTML = "GAME STARTED";
 
         for (var i = 0; i < boundaries.length; i++) {
-            boundaries[i].addEventListener("mouseover", you_lost, true);
+            boundaries[i].addEventListener("mouseover", you_lost);
         }
 
-        if (end.addEventListener("mouseover", you_won, true));
-    }
-
+        if (end.addEventListener("mouseover", you_won));
+}
 var you_won = function () {
      
     document.body.style.backgroundColor = "lightgreen";
     game_prompt.innerHTML = "YOU WON! Press Space To restart";
     score += 5;
     console.log(score);
-    not_playing = true;
-    end.removeEventListener("mouseover", you_won, false);
+    finished = true;
     }
 
 var you_lost = function () {
     for (var i = 0; i < boundaries.length; i++) {
-    boundaries[i].classList.add("youlose");// https://stackoverflow.com/a/14101453
+        boundaries[i].classList.add("youlose");// https://stackoverflow.com/a/14101453
   }
+    document.body.style.backgroundColor = "#ff8888";
     game_prompt.innerHTML = "YOU LOST :( Press Space to restart";
     score -= 10;
     console.log(score);
-    not_playing = true;
-    
-    for (var i = 0; i < boundaries.length; i++) {
-      boundaries[i].removeEventListener("mouseover", you_lost, false);
+    finished=true;
     }
-    }
+
+
 
 var reset = function () {
     for (var i = 0; i < boundaries.length; i++) {
@@ -74,6 +76,7 @@ document.addEventListener("keyup", (reset) => {
 });
 
 start.addEventListener('click', ()=>{ //https:stackoverflow.com/a/25028877
+    
     reset();
     run_maze ();
 });
