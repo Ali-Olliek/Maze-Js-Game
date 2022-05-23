8b42d79ed090230f69c6b309b5cdf7deebb0b1b8
AliOlliek <olliek95@gmail.com>
2022-05-23 23:33:09 +0300

    JS -- Stopwatch stops when round stops

--- game.js
+++ game.js
@@ -1,114 +1,101 @@
 window.onload = function () {
+  // Graphical Elements
+  var boundaries = document.getElementsByClassName("boundary");
+  var start = document.getElementById("start");
+  var game_prompt = document.getElementById("status");
+  var end = document.getElementById("end");
+  var game = document.getElementById("game");
+  var score_display_list = document.getElementsByClassName("example");
+  var score_display = score_display_list[0];
+  // Timers
+  var now = document.getElementById("now");
+  var last = document.getElementById("last");
+  var best = document.getElementById("best");
 
-    var boundaries = document.getElementsByClassName("boundary");
-    var start = document.getElementById("start");
-    var game_prompt = document.getElementById("status");
-    var end = document.getElementById("end");
-    var score = 0;
-    var game = document.getElementById("game");
-    var score_display_list = document.getElementsByClassName("example");
-    var score_display = score_display_list[0];
-<<<<<<< HEAD
-    var now = document.getElementById("now");
-    var last = document.getElementById("last");
-    var best = document.getElementById("best");
-=======
-    playing = false;
->>>>>>> d3182c792bc3e3160a2cb1b2b5378095fd4e9da3
+  var score = 0;
 
-var run_maze = function (){
+  var run_maze = function () {
     for (var i = 0; i < boundaries.length; i++) {
-        boundaries[i].addEventListener("mouseover", you_lost);  
+      boundaries[i].addEventListener("mouseover", you_lost);
     }
-    var seconds=0;
-    var timer = setInterval(function () {
-        now.innerHTML = seconds++;
-    }, 100);
-
     game.addEventListener("mouseleave", you_lost);
 
     game_prompt.innerHTML = "GAME STARTED";
     end.addEventListener("mouseover", you_won);
-}
+    stopwatch();
+  };
 
-
-var you_won = function () {
+  var you_won = function () {
+    playing = false;
     document.body.style.backgroundColor = "lightgreen";
     game_prompt.innerHTML = "YOU WON! Press Space To restart";
     score += 5;
-    score_display.innerHTML = "SCORE: " + score
-    remove_listeners()
-    clearInterval(timer);
-    }
+    score_display.innerHTML = "SCORE: " + score;
+    stop_timer();
+    remove_listeners();
+  };
 
-var you_lost = function () {
-    
+  var you_lost = function () {
+    playing = false;
     score -= 10;
     document.body.style.backgroundColor = "#ff8888";
     game_prompt.innerHTML = "YOU LOST :( Press Space to restart";
-    score_display.innerHTML = "SCORE: " + score
-    remove_listeners()
-    clearInterval(timer)
-}
+    score_display.innerHTML = "SCORE: " + score;
+    stop_timer();
+    remove_listeners();
+  };
 
-// GET RID OF UNWANTED LISTENERS
+  // GET RID OF UNWANTED LISTENERS
 
-var remove_listeners = function () {
-
-    for (var i = 0; i < boundaries.length; i++) { 
-        boundaries[i].removeEventListener("mouseover", you_lost);
+  var remove_listeners = function () {
+    for (var i = 0; i < boundaries.length; i++) {
+      boundaries[i].removeEventListener("mouseover", you_lost);
     }
     game.removeEventListener("mouseleave", you_lost);
     end.removeEventListener("mouseover", you_won);
-    
-}
+  };
 
-// Calculate time
-var get_time = function() {
-    if(playing) {
-    let time=0;
-    setInterval((e) => {
-        time++
-        console.log(time)
-    }, 500);
-}
-}
+  function stop_timer() {
+    clearInterval(timer);
+  }
 
-// Display Time
-var current_time = document.getElementById('now').innerHTML= "This round: "+ get_time();
-var best_time = document.getElementById('best').innerHTML = "Best round: " + get_time();
-var last = document.getElementById('last').innerHTML = "Last round: " + get_time();
-// GAME RESET
+  // Time
 
-var reset = function () {
+  function stopwatch() {
+      var sec = 0;
+      timer = setInterval(() => {
+        now.innerHTML = "00:" + sec;
+        sec++;
+      }, 100);
+  }
+
+  // GAME RESET
+
+  var reset = function () {
     for (var i = 0; i < boundaries.length; i++) {
       boundaries[i].style.backgroundColor = "white";
     }
-    
+
     document.body.style.backgroundColor = "white";
     game_prompt.innerHTML = 'Begin by moving your mouse over the "S"';
+  };
 
+  // SPACEBAR RESET
+
+  document.addEventListener("keyup", (e) => {
+    if (e.code === "Space") {
+      stopwatch = 0;
+      score = 0;
+      reset();
     }
+  });
 
-// SPACEBAR RESET
+  // START GAME
 
-document.addEventListener("keyup", (e) => { 
-    if (e.code === "Space" && playing==false) {
-    reset()
-    }
-});
-
-
-// Timer
-
-
-// START GAME
-
-start.addEventListener('click', ()=>{ //https:stackoverflow.com/a/25028877
-    if(!playing) {
-    playing=true;
+  start.addEventListener("click", () => {
+    //https:stackoverflow.com/a/25028877
+    playing = true;
     reset();
-    run_maze ();
-}
-});
+    run_maze();
+  });
 };
